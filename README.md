4d54ee67e6ff3a041f4c2be3b3f96a3ca316088a
Dan Mindru <mindrudan@gmail.com>
2015-09-18 23:22:05 +0200

    Initial code

---
+++ README.md
@@ -0,0 +1,41 @@
+# Angular collapsible block / ng-transclude demo
+![Angular collapsible block]([UPDATE DEMO IMAGE URL])
+
+
+### [Quick Demo](http://ng-milk.github.io/angular-collapsible-block-directive/)
+A directive that uses ng-transclude to create collapsible blocks. Read more about it [here]([UPDATE BLOG POST URL}]).
+
+
+## Usage
+1. Include `ng-collapsible-block.js`.
+2. Add `dm.collapsibleBlock` as a dependency to your app.
+3. Profit!
+
+...
+4. (optional) Include `ng-collapsible-block.css` if you want to use `ngAnimate`.
+5. (optional) Include `ngAnimate` as a dependency to animate the help block toggling.
+
+
+## Bower
+Installable via `bower`:
+
+```bash
+bower install ng-collapsible-block
+```
+
+## Example
+See [index.html](https://github.com/ng-milk/angular-collapsible-block-directive/blob/master/index.html) for an example.
+
+```html
+!!!!!!!!!!!!!!!!!!!@TODO
+```
+
+
+## About ngmilk
+<img src="http://ngmilk.rocks/content/images/2014/10/111-1.jpg" width="200px"/>
+
+**ngmilk** is the place to go for fresh front-end articles, with a focus on AngularJS.
+See more on [ngmilk.rocks](https://ngmilk.rocks)
+
+Follow [@ngmilkrocks](http://twitter.com/ngmilkrocks) on Twitter to stay ahead of the game.
+
---
+++ src/index.js
@@ -0,0 +1,46 @@
+/*
+ * angular-collapsible-block v0.0.1
+ * (c) 2015 Dan Mindru http://mindrudan.com
+ * License: MIT
+ */
+
+angular.module('dm.collapsibleBlock', [])
+       .directive('collapsible', collapsibleBlock);
+
+
+/*
+ * @param <String> attrs.title: the title will be displayed by default
+ * @param <String> attrs.collapsedIcon: the class for the title icon when collapsed (no icon default).
+ * @param <String> attrs.expandedIcon: the class for the title icon when expanded (no icon default).
+ */
+function collapsibleBlock(){
+  function collapsibleLink(scope, element, attrs){
+    scope.collapsed = true;
+    scope.title = attrs.collapsibleTitle;
+    scope.collapsedIcon = attrs.collapsedIcon
+    scope.expandedIcon = attrs.expandedIcon
+
+    scope.toggle = function collapsibleToggle(e){
+      e.preventDefault();
+      scope.collapsed = !scope.collapsed;
+    };
+  }
+
+  return {
+    scope: {},
+    transclude: true,
+    restrict: 'E',
+    replace: true,
+    template: '<section class="clps" ng-class="{\'clps--expanded\': collapsed}">
+                <a href="#" class="clps__title" ng-click="toggle($event)">
+                  <i class="{{collapsedIcon}}" ng-if="collapsedIcon && collapsed"></i>
+                  <i class="{{expandedIcon}}" ng-if="expandedIcon && !collapsed"></i>
+
+                  <span translate>{{title}}</span>
+                </a>
+
+                <ng-transclude class="clps__content" ng-show="!collapsed"></ng-transclude>
+              </section>',
+    link: collapsibleLink
+  }
+}
